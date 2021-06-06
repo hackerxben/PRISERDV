6935ecaf96f3e769e282da38f16f9d520d3886fc
Jihed <hackerxben@gmail.com>
2021-06-06 17:32:30 +0100

    Bug Fixed

--- src/app/show-calendar/show-calendar.component.ts
+++ src/app/show-calendar/show-calendar.component.ts
@@ -99,29 +99,46 @@ export class ShowCalendarComponent implements OnInit, AfterViewInit {
   }
 
 
+  getAppointmentsOfDoctorPerDate(doctor: User, date: string): Appointment[]{
+    return this.appointments.filter(
+      function(appointment){
+        return appointment.doctor!.login === doctor.login && appointment.dateStart!.toString().indexOf(date) != -1
+      }
+    )
+  }
+
+  getAvailableTimeSlots(doctor: User, date: string): string[] {
+    let availableTimeSlots: string[] = []
+    // doctor has no appointments in that date so return all availableTimeSlots
+    const appointmentsOfDoctorInDate = this.getAppointmentsOfDoctorPerDate(doctor,date)
+    if(appointmentsOfDoctorInDate.length === 0){
+      // console.log("Doctor: "+doctor.login +" has no appointments you can pick any timeslot")
+      availableTimeSlots = Object.values(TIME_SLOT)
+    }else{
+      // console.log("Appointments of Doctor on "+date+ " : "+JSON.stringify(appointmentsOfDoctorInDate))
+      let reservedTimeSlots:string[] = []
+      appointmentsOfDoctorInDate.forEach(
+        a => {
+          const start = a.dateStart!.toString().split("T")[1].split(":")[0]+":00"
+          const end = a.dateEnd!.toString().split("T")[1].split(":")[0]+":00"
+          reservedTimeSlots.push(start+" => "+end)
+        }
+      )
+      // console.log("Doctor: "+doctor.login +" has appointments on these timeslots: " + reservedTimeSlots + " on "+date)
+      availableTimeSlots = Object.values(TIME_SLOT).filter(t =>reservedTimeSlots.indexOf(t)===-1)
+      // console.log("Available TimeSlots for doctor: "+doctor.login+" on "+date+" : "+availableTimeSlots)
+    }
+    return availableTimeSlots
+  }
+
   onDoctorChange(event: any): void {
     const doctor = this.doctors.filter(d => d.login === event.target.value)[0]
     if(doctor){
       this.availableTimeSlots = this.getAvailableTimeSlots(doctor, this.selectInfo.startStr)
     }else{
-      console.log("doctor not found:")
+      // console.log("doctor not found:")
       this.availableTimeSlots = Object.values(TIME_SLOT)
     }    
-  }
-
-  doctorHasAppointmentInTimeSlot(timeSlot: any, doctor: User): boolean {
-    const date = this.selectInfo.startStr
-    return this.appointments.filter(
-      function (appointment) {
-        const dateStart = new Date(appointment.dateStart!).toISOString()
-        const dateEnd = new Date(appointment.dateEnd!).toISOString()
-        let docHasAppointment = appointment.doctor!.login === doctor.login && dateStart.indexOf(date) != -1
-        const startHour = dateStart.split("T")[1].split(":")[0]
-        const endHour = dateEnd.split("T")[1].split(":")[0]
-        docHasAppointment = docHasAppointment && timeSlot === startHour + "h => " + endHour + "h"
-        return docHasAppointment
-      }
-    ).length > 0
   }
 
   openAddAppointmentModal(content: any, selectInfo: any): void {
@@ -140,7 +157,7 @@ export class ShowCalendarComponent implements OnInit, AfterViewInit {
         appointment.parent = this.loggedInUser
         this.addAppointmentToCalendar(appointment)
         this.appointments.push(appointment)
-        console.log("new appointments:"+ JSON.stringify(this.appointments))
+        // console.log("new appointments:"+ JSON.stringify(this.appointments))
         this.selectedDoctor = "Select a Doctor"
         this.selectedTimeSlot = "Select Time Slot"
         //save new appointment in backend
@@ -160,38 +177,7 @@ export class ShowCalendarComponent implements OnInit, AfterViewInit {
 
 
 
-  getAvailableTimeSlots(doctor: User, date: string): string[] {
-    if (this.appointments) {
-      const appointmentsOfDoctor = this.appointments.filter(
-        function (appointment) {
-          return appointment.doctor!.login === doctor.login && appointment.dateStart!.toString().indexOf(date) != -1
-        }
-      )
-
-      if (appointmentsOfDoctor.length === 0) {
-        console.log("no appointments for doctor")
-        return Object.values(TIME_SLOT)
-      }
-      let timeSlots = Object.values(TIME_SLOT)
-      const avaialbleTimeSlots = timeSlots.filter(
-        function (timeSlot) {
-          return appointmentsOfDoctor.filter(
-            (appointment) => {
-              const hourFromDate = appointment.dateStart!.toString().split("T")[1]
-              const hourTimeSlot = timeSlot.split(" => ")[0]
-              const condition = !(hourFromDate.indexOf(hourTimeSlot) != -1)
-              return condition
-            }
-          ).length !== 0
-        }
-      )
-      return avaialbleTimeSlots
-    }else{
-      console.log("no appointments")
-      return Object.values(TIME_SLOT)
-    }
-    
-  }
+  
 
   addAppointmentToCalendar(appointment: Appointment) {
     let dateStart = new Date().toISOString()
